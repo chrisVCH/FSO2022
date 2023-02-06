cd34332444f8ce6f4d7436f4b5913ebd3bf4a172
Chris Chen <chrischen.mc2@gmail.com>
2023-02-06 15:14:49 -0800

    Delete Persons.js

--- part2/countries/src/components/Persons.js
+++
@@ -1,7 +0,0 @@
-  const Persons = ({filteredpersons, handleDelete})=> {
-    return (
-      filteredpersons.map(n => <div key={n.id}>{n.name} {n.number} <button onClick={ () => handleDelete(n.id) }>delete</button></div>)
-    )
-  }
-
-  export default Persons
